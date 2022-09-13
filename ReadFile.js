bb6d32fa8954a7db47e84c033784291efd0c8906
Bernardo Meyer <55299005+b3rn4rd0m3y3r@users.noreply.github.com>
2022-09-12 23:59:56 -0300

    Add files via upload

--- ReadFile.js
+++ ReadFile.js
@@ -2,16 +2,18 @@ var app = require('http').createServer(resposta);
 var fs = require('fs');
 
 app.listen(3000);
+//app.listen(process.env.PORT);
 console.log("Aplicação está em execução...");
+
 function resposta (req, res) {
-     fs.readFile(__dirname + '/index.html',
+     fs.readFile(__dirname + '/exemplo.html',
      function (err, data) {
          if (err) {
               res.writeHead(500);
-              return res.end('Erro ao carregar o arquivo index.html');
-         }
+              return res.end('Erro ao carregar o arquivo exemplo.html');
+		}
 
-         res.writeHead(200);
+         res.writeHead(200, {'Content-Type': 'text/html'});
          res.end(data);
      });
 }
