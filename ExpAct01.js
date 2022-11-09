2287e20dfa3b5c7acd3ac05018e99484c1896f1d
Bernardo Meyer <55299005+b3rn4rd0m3y3r@users.noreply.github.com>
2022-11-08 22:29:56 -0300

    Add files via upload

--- ExpAct01.js
+++ ExpAct01.js
@@ -4,16 +4,17 @@
  //var app = express.createServer();
  const app = express();
 
- //app.use(express.json());
+ app.use(express.json());
  app.use(express.urlencoded({ extended: true }));
 
  app.post('/form', function (req, res) {
   var retorno = "";
-  //res.statusCode = 200;
-  //res.setHeader('Content-Type', 'text/html');   
+   res.statusCode = 200;
+   res.setHeader('Content-Type', 'application/json');   
    retorno = req.body;
    console.log(retorno);
    res.send(retorno);
  });
  
  app.listen(3008);
+ console.log("Listen on port 3008.");
